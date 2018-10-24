e0d04fcd5c41af283425f8e63716d0753e01e957
Karolina_Kantorska <karolina.kantorska@gmail.com>
2018-10-24 19:25:20 +0200

    finnal commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
+
+// global variables
 let win = false;
 let allEnemies= [];
+let colision = false;
 // This JavaScript function always returns a random number between min (included) and max (excluded):
 // https://www.w3schools.com/js/js_random.asp
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
-// Enemies our player must avoid
+
+// Enemies our player must avoid (enemies prototype function)
+/**
+* @description Enemies constructor
+* @constructor
+* @param {number} this.x - x position of an object
+* @param {number} this.y - y position of an object
+* @param {number} this.speed - speed of an object
+* @param {string} this.sprite - object image reference
+*/
 var Enemy = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -17,8 +27,9 @@ var Enemy = function(x,y,speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position
+// TODO: add other fruits
+// @param {number} dt, a time delta between ticks
 Enemy.prototype.update = function(dt, speed) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -26,92 +37,143 @@ Enemy.prototype.update = function(dt, speed) {
     this.x= this.x +this.speed *dt;
 };
 
-// Draw the enemy on the screen, required method for game
+// TODO:Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-var Player = function () {
-  Enemy.call(this);
-  this.x = 202;
-  this.y = 463;
-  this.sprite = 'images/char-boy.png';
-  this.speed= 0;
-}
+/**
+* @description player class constructor
+* @constructor
+* @param {number} this.x - x position of an object
+* @param {number} this.y - y position of an object
+* @param {number} this.speed - speed of an object
+* @param {string} this.sprite - object image reference
+*/
 
+var Player = function () {
+    Enemy.call(this);
+    this.x = 202;
+    this.y = 463;
+    this.sprite = 'images/char-boy.png';
+    this.speed= 0;
+}
+// TODO: player gets a prototype enemy to use the render function of enemy
 Player.prototype = Object.create(Enemy.prototype);
 Player.prototype.constructor = Player;
-// This class requires an update(), render() and
-// a handleInput() method.
 
+
+// player update() methode
+// TODO: sets a winning condition
 Player.prototype.update = function() {
+    if (player.y === -17){
+      win = true;
+    }
+};
+
+//player handleInput() methode
+// TODO: moves the player after pressing the arrow keys
+Player.prototype.handleInput = function() {
+    window.onkeyup = function(e) {
+      var key = e.keyCode ? e.keyCode : e.which;
+
+      if (key == 37 && player.x > 2) {  // left
+        player.x = player.x -100;
+      }
+      else if (key == 38 && player.y  >0) { // up
+        player.y = player.y -80;
+      }
+      else if (key == 39 && player.x < 402) {  // right
+        player.x = player.x +100;
+      }
+      else if (key == 40 && player.y < 383) { // down
+        player.y = player.y +80
+      }
+    }
+}
+
+// TODO: generates how high will run the enemy
+// from 3 possible highness: 63, 143, 223
+function enemyY(){
+    return 63+80*getRndInteger(0,3);
+};
+
+// TODO: check collision beteen player and enemy
+// checks if player and enemy are in the same rows
+// checks if x positions of player and enemy overlaps
+// (with margin of 25 pixels)
+// if yes the vert of collision bolean is changed into 'true'
+function checkCollisions(){
+    allEnemies.forEach(function(enemy){
+      if (player.y === enemy.y &&
+        player.x + 25 < enemy.x + 101 &&
+        player.x + 75 > enemy.x){
+        colision = true;
+      }
+    });
+  };
+
+// TODO: reloads the game
+function playerReset(){
+    setTimeout(function () {
+      location.reload();
+    }, 500);
+};
+
+// TODO: reloads the game in case of failure
+function failure(){
+    if (colision === true){
+      playerReset ();
+    }
+};
+
+// TODO: displays the 'Congratulations' message
+// restarts the game
+function  winningMessage (){
+    document.querySelector('.modalContainer').style.display = 'block';
+    setTimeout(function () {
+      playerReset ();
+    }, 4000);
 
 };
 
-
-Player.prototype.handleInput = function() {
-  window.onkeyup = function(e) {
-     var key = e.keyCode ? e.keyCode : e.which;
-
-     if (key == 37) {  // left
-         player.x = player.x -100;
-     }
-     else if (key == 38) { // up
-         player.y = player.y -80;
-     }
-     else if (key == 39) {  // right
-         player.x = player.x +100;
-     }
-     else if (key == 40) { // down
-         player.y = player.y +80
-     }
-     }
-}
-
-
-function enemyY(){
-  return 63+80*getRndInteger(0,3);
-}
-/*
-function colisions 
-function collides(a, b)
-{
-    if (a.x < b.x + b.width &&
-        a.x + a.width > b.x &&
-        a.y < b.y + b.height &&
-        a.y + a.height > b.y) return true;
-}
-*/
-
-// Now instantiate your objects.
-// instantiate enemies
-// Place all enemy objects in an array called allEnemies
+// TODO: funktion instantiating the enemies objects.
+// Placing all enemy objects in an array called allEnemies
+// limiting the length of array to 15
 function enemysAttack (){
-  if (win === false){
-    let enemy1 = new Enemy(-200, enemyY() ,getRndInteger(20, 120));
-    allEnemies.unshift(enemy1);
-    allEnemies.length = 15;
+    if (win === false){
+      let enemy1 = new Enemy(-200, enemyY() ,getRndInteger(60, 160));
+      allEnemies.unshift(enemy1);
+      allEnemies.length = 15;
+      }
+      else {
+        winningMessage ();
     }
 }
-enemysAttack ();
-  setInterval(function (){
-enemysAttack ();
-}, 2000);
 
-// Place the player object in a variable called player
+// calling the function enemysAttack
+enemysAttack ();
+enemysAttack ();
+enemysAttack ();
+// calling the function enemysAttack with the time interval
+setInterval(function (){
+    enemysAttack ();
+}, 1000);
 
+
+// Placing the player object in a variable called player
 let player= new Player();
+
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
